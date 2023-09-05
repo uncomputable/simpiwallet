9138a3b7d22410c1870f8b0cd926e2bc44e4ab5d
Christian Lewe <ch_lewe@agdsn.me>
2023-09-05 13:52:08 +0200

    Descriptor: Import asm fragment

--- src/descriptor.rs
+++ src/descriptor.rs
@@ -6,9 +6,16 @@ use miniscript::{Descriptor, MiniscriptKey};
 
 use crate::key::UnspendableKey;
 
-pub fn simplicity_pk<Pk: MiniscriptKey + UnspendableKey>(pk: Pk) -> Descriptor<Pk> {
+pub fn simplicity_pk<Pk: MiniscriptKey + UnspendableKey>(key: Pk) -> Descriptor<Pk> {
     let internal_key = Pk::unspendable();
-    let policy = simplicity::Policy::Key(pk);
+    let policy = simplicity::Policy::Key(key);
+    let tree = TapTree::SimplicityLeaf(Arc::new(policy));
+    Descriptor::new_tr(internal_key, Some(tree)).expect("single leaf is within bounds")
+}
+
+pub fn simplicity_asm<Pk: MiniscriptKey + UnspendableKey>(cmr: simplicity::Cmr) -> Descriptor<Pk> {
+    let internal_key = Pk::unspendable();
+    let policy = simplicity::Policy::Assembly(cmr);
     let tree = TapTree::SimplicityLeaf(Arc::new(policy));
     Descriptor::new_tr(internal_key, Some(tree)).expect("single leaf is within bounds")
 }
