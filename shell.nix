d2ebd904c163d673af5f3cd0a31e9a463282ae2f
Christian Lewe <ch_lewe@agdsn.me>
2023-09-05 13:32:33 +0200

    Switch to simplicityregtest1
    This is the second testnet for Simplicity.

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 with (import <nixpkgs> {});
 let
   elementsd-simplicity = elementsd.overrideAttrs (_: rec {
-    version = "unstable-2023-06-15";
+    version = "unstable-2023-08-24";
     src = fetchFromGitHub {
       owner = "ElementsProject";
       repo = "elements";
-      rev = "80c5d581ff2a5c9d0d2f759f84575a9fe8204efa"; # <-- update this to latest `simplicity` branch: https://github.com/ElementsProject/elements/commits/simplicity
+      rev = "16fd80c3aca58e059268a11a3cba5f3c0d2607a2"; # <-- update this to latest `simplicity` branch: https://github.com/ElementsProject/elements/commits/simplicity
       sha256 = "ooe+If3HWaJWpr2ux7DpiCTqB9Hv+aXjquEjplDjvhM="; # <-- ignore this
     };
   });
