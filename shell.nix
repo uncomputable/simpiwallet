7811f2b6423391b8a4fe73544324354184f5e210
Christian Lewe <ch_lewe@agdsn.me>
2023-09-19 21:32:05 +0200

    Nix: Fix shell
    The missing sha hash caused nix to use the old source (with the same
    hash) instead of fetching the new source from github. From now on, we
    have to update the hash along with the git rev.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,12 @@
 with (import <nixpkgs> {});
 let
   elementsd-simplicity = elementsd.overrideAttrs (_: rec {
-    version = "unstable-2023-08-24";
+    version = "unstable-2023-08-24a";
     src = fetchFromGitHub {
       owner = "ElementsProject";
       repo = "elements";
       rev = "16fd80c3aca58e059268a11a3cba5f3c0d2607a2"; # <-- update this to latest `simplicity` branch: https://github.com/ElementsProject/elements/commits/simplicity
-      sha256 = "ooe+If3HWaJWpr2ux7DpiCTqB9Hv+aXjquEjplDjvhM="; # <-- ignore this
+      sha256 = "sha256-Sv3kMnlnXXmCYshSPwTkgEc/M6aHAQInnqCB7kXW4WY="; # <-- overwrite this, rerun and place the expected hash
     };
   });
 in
